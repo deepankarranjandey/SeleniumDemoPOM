ac0101758acde032142b9876b1f223092f1d45a5
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-23 13:34:01 +0530

    struct changes

---
+++ src/test/java/testcases/UserTest.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UserTest {
+
+
+    @Test
+    public void userRegistrationfunctionality()
+    {
+        WebDriver driver=new ChromeDriver();
+    }
+
+}
