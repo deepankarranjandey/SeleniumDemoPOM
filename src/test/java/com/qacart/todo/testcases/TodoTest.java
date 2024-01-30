a6f933aa90623982247d24941b4bdef440b21305
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-30 17:35:46 +0530

    POJO class added

--- src/test/java/com/qacart/todo/testcases/TodoTest.java
+++ src/test/java/com/qacart/todo/testcases/TodoTest.java
@@ -3,6 +3,7 @@ package com.qacart.todo.testcases;
 import com.github.javafaker.Faker;
 import com.qacart.todo.base.BaseTest;
 import com.qacart.todo.factory.DriverFactory;
+import com.qacart.todo.models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,13 +23,14 @@ public class TodoTest extends BaseTest {
         String  lastName=faker.name().lastName();
         String email=faker.internet().safeEmailAddress();
 
+        User user=new User(firstName,lastName,email,"Test@123");
 
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys(firstName);
-        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(email);
-        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys("Test@123");
-        driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(user.getEmail());
+        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys(user.getPassword());
+        driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys(user.getPassword());
         driver.findElement(By.cssSelector("button[type='button']")).click();
         boolean isWelcomeDisplayed = driver.findElement(By.cssSelector("h2[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeDisplayed);
@@ -48,13 +50,14 @@ public class TodoTest extends BaseTest {
         String  lastName=faker.name().lastName();
         String email=faker.internet().safeEmailAddress();
 
+        User user=new User(firstName,lastName,email,"Test@123");
 
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys(firstName);
-        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(email);
-        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys("Test@123");
-        driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(user.getEmail());
+        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys(user.getPassword());
+        driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys(user.getPassword());
         driver.findElement(By.cssSelector("button[type='button']")).click();
         boolean isWelcomeDisplayed = driver.findElement(By.cssSelector("h2[data-testid='welcome']")).isDisplayed();
         Assert.assertTrue(isWelcomeDisplayed);
--- src/test/java/com/qacart/todo/testcases/UserTest.java
+++ src/test/java/com/qacart/todo/testcases/UserTest.java
@@ -1,6 +1,9 @@
 package com.qacart.todo.testcases;
 
+import com.github.javafaker.Faker;
+import com.qacart.todo.base.BaseTest;
 import com.qacart.todo.factory.DriverFactory;
+import com.qacart.todo.models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,19 +12,24 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class UserTest {
+public class UserTest extends BaseTest {
 
 
     @Test
     public void userRegistrationfunctionality()
     {
-        WebDriver driver=new DriverFactory().initializeDriver();
+        Faker faker=new Faker();
+        String  firstName=faker.name().firstName();
+        String  lastName=faker.name().lastName();
+        String email=faker.internet().safeEmailAddress();
+
+        User user=new User(firstName,lastName,email,"Test@123");
 
         driver.get("https://todo.qacart.com/signup");
-        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys("Deepankar");
-        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys("Dey");
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("deepankar2@yopmail.com");
-        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys(user.getFirstName());
+        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys(user.getLastName());
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(user.getEmail());
+        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys(user.getPassword());
         driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys("Test@123");
         driver.findElement(By.cssSelector("button[type='button']")).click();
         boolean isWelcomeDisplayed = driver.findElement(By.cssSelector("h2[data-testid='welcome']")).isDisplayed();
