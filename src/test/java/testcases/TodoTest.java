b9266d4ad938382298e383398ec77d516b3349c0
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-30 16:26:43 +0530

    test cases added

---
+++ src/test/java/testcases/TodoTest.java
@@ -0,0 +1,51 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TodoTest {
+
+
+    @Test(priority = 1)
+    public void shouldBeAbletoAddTodo()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://todo.qacart.com/login");
+        driver.findElement(By.cssSelector("#email")).sendKeys("deepankar1@yopmail.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String headerText= driver.findElement(By.cssSelector("h2[data-testid='welcome']")).getText().toLowerCase();
+        Assert.assertEquals(headerText,"good afternoon deepankar");
+        driver.findElement(By.cssSelector(".MuiIconButton-label")).click();
+        driver.findElement(By.cssSelector("input[data-testid='new-todo']")).sendKeys("Deep Test");
+        driver.findElement(By.cssSelector("button[data-testid='submit-newTask']")).click();
+        String todoText= driver.findElement(By.cssSelector("h2[data-testid='todo-text']")).getText();
+        Assert.assertEquals(todoText,"Deep Test");
+        driver.quit();
+    }
+
+    @Test(priority = 2)
+    public void deleteTodo()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://todo.qacart.com/login");
+        driver.findElement(By.cssSelector("#email")).sendKeys("deepankar1@yopmail.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String headerText= driver.findElement(By.cssSelector("h2[data-testid='welcome']")).getText().toLowerCase();
+        Assert.assertEquals(headerText,"good afternoon deepankar");
+        driver.findElement(By.xpath("(//span[@class='MuiIconButton-label'])[3]")).click();
+        String checkTodo = driver.findElement(By.cssSelector("h4[data-testid='no-todos']")).getText();
+        Assert.assertEquals(checkTodo,"No Available Todos");
+        driver.quit();
+    }
+}
--- src/test/java/testcases/UserTest.java
+++ src/test/java/testcases/UserTest.java
@@ -1,8 +1,12 @@
 package testcases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class UserTest {
 
@@ -11,6 +15,18 @@ public class UserTest {
     public void userRegistrationfunctionality()
     {
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://todo.qacart.com/signup");
+        driver.findElement(By.cssSelector("input[data-testid='first-name']")).sendKeys("Deepankar");
+        driver.findElement(By.cssSelector("input[data-testid='last-name']")).sendKeys("Dey");
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("deepankar2@yopmail.com");
+        driver.findElement(By.cssSelector("input[data-testid='password']")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("input[data-testid='confirm-password']")).sendKeys("Test@123");
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        boolean isWelcomeDisplayed = driver.findElement(By.cssSelector("h2[data-testid='welcome']")).isDisplayed();
+        Assert.assertTrue(isWelcomeDisplayed);
+        driver.quit();
     }
 
 }
