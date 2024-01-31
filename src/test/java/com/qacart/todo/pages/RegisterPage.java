432417237391b82d58e6bdaea130ea33e5b88554
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-31 13:46:54 +0530

    POM class updated and reformated

--- src/test/java/com/qacart/todo/pages/RegisterPage.java
+++ src/test/java/com/qacart/todo/pages/RegisterPage.java
@@ -8,28 +8,26 @@ public class RegisterPage {
 
     // elements
 
-    private final By firstNameInput= By.cssSelector("input[data-testid='first-name']");
-    private final By lastNameInput= By.cssSelector("input[data-testid='last-name']");
-    private final By emailInput= By.cssSelector("input[type='email']");
-    private final By passwordInput= By.cssSelector("input[data-testid='password']");
-    private final By confirmPasswordInput= By.cssSelector("input[data-testid='confirm-password']");
+    private final By firstNameInput = By.cssSelector("input[data-testid='first-name']");
+    private final By lastNameInput = By.cssSelector("input[data-testid='last-name']");
+    private final By emailInput = By.cssSelector("input[type='email']");
+    private final By passwordInput = By.cssSelector("input[data-testid='password']");
+    private final By confirmPasswordInput = By.cssSelector("input[data-testid='confirm-password']");
 
-    private final By submitButton=By.cssSelector("button[type='button']");
+    private final By submitButton = By.cssSelector("button[type='button']");
 
 
     // constructor
 
 
-
     // methods
-public void register (WebDriver driver, User user)
-{
-    driver.findElement(firstNameInput).sendKeys(user.getFirstName());
-    driver.findElement(lastNameInput).sendKeys(user.getLastName());
-    driver.findElement(emailInput).sendKeys(user.getEmail());
-    driver.findElement(passwordInput).sendKeys(user.getPassword());
-    driver.findElement(confirmPasswordInput).sendKeys(user.getPassword());
-    driver.findElement(submitButton).click();
-}
+    public void register(WebDriver driver, User user) {
+        driver.findElement(firstNameInput).sendKeys(user.getFirstName());
+        driver.findElement(lastNameInput).sendKeys(user.getLastName());
+        driver.findElement(emailInput).sendKeys(user.getEmail());
+        driver.findElement(passwordInput).sendKeys(user.getPassword());
+        driver.findElement(confirmPasswordInput).sendKeys(user.getPassword());
+        driver.findElement(submitButton).click();
+    }
 
 }
